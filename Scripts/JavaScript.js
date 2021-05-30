939acb1a21bdb7e7f84f82d097abc09e4d4603eb
vlad330d <vlad330d@yandex.ru>
2021-05-30 15:52:05 +0100

    ++

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,20 +1,18 @@
 let popup = document.querySelector (".popup");
 let popupOpen = document.querySelector (".edit-button");
 let popupClose = document.querySelector (".popup__close-button");
-
-popupOpen.addEventListener ("click", toggleClass);
-popupClose.addEventListener ("click", toggleClass);
-
- function toggleClass (){
-    popup.classList.toggle ("popup_opened")
- }
-
 let userName = document.querySelector (".user__name");
 let profession = document.querySelector (".user__profession");
-
 let formElement = document.querySelector (".popup__container");
 let nameInput = document.querySelector (".popup__name");
 let jobInput = document.querySelector (".popup__profession");
+
+ function toggleClass (){
+   popup.classList.toggle ("popup_opened")
+ }
+ 
+popupOpen.addEventListener ("click", toggleClass);
+popupClose.addEventListener ("click", toggleClass);
 
 nameInput.setAttribute ("value", userName.textContent);
 jobInput.setAttribute ("value", profession.textContent);
