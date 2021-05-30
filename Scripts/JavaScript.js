a5fc89851e786d489d15ef68573745979a0103c5
vlad330d <vlad330d@yandex.ru>
2021-05-30 14:59:39 +0100

    js

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,6 +1,6 @@
 let popup = document.querySelector (".popup");
 let popupOpen = document.querySelector (".edit-button");
-let popupClose = document.querySelector (".popup__close-button")
+let popupClose = document.querySelector (".popup__close-button");
 
 popupOpen.addEventListener ("click", toggleClass);
 popupClose.addEventListener ("click", toggleClass);
@@ -8,3 +8,25 @@ popupClose.addEventListener ("click", toggleClass);
  function toggleClass (){
     popup.classList.toggle ("popup_opened")
  }
+
+let userName = document.querySelector (".user__name");
+let profession = document.querySelector (".user__profession");
+
+let formElement = document.querySelector (".popup__container");
+let nameInput = document.querySelector (".popup__name");
+let jobInput = document.querySelector (".popup__profession");
+
+nameInput.setAttribute ("value", userName.textContent);
+jobInput.setAttribute ("value", profession.textContent);
+
+function formSubmitHandler (evt) {
+   evt.preventDefault();
+   
+   nameInput.value;
+   jobInput.value;
+
+   userName.textContent = nameInput.value;
+   profession.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
